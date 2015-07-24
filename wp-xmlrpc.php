cbe76d68c2a64efeb286e26a8f7cbc0326c8aff9
syg <sgogel@ecedi.fr>
2015-07-24 11:26:03 +0200

    fix mu plugin name

--- wp-xmlrpc.php
+++ wp-xmlrpc.php
@@ -4,9 +4,9 @@
  * @package Wordpress
  * @license http://opensource.org/licenses/MIT MIT
  *
- * Plugin Name: disable-xmlrpc
+ * Plugin Name: wp-xmlrpc
  * Description: Disable xmlrpc api
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Sylvain Gogel <sgogel@ecedi.fr>
  * Author URI: http://ecedi.fr
  */
